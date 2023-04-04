bc409da6b500de9dbe7d560132fe97b8932f4c01
karthik61 <karthik@aspectsites.com>
2023-04-04 21:58:32 +0530

    ui enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,11 @@ function App() {
   function addToList() {
     if (inputValue.length > 0) {
       setTodolist([...todolist, inputValue]);
-    setInputValue("");
+      setInputValue("");
     }
-    
   }
   function deleteItem(index) {
-    setTodolist(prevTodolist => {
+    setTodolist((prevTodolist) => {
       const newTodolist = [...prevTodolist];
       newTodolist.splice(index, 1);
       return newTodolist;
@@ -29,45 +28,53 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <section className="mt-3 mb-3">
-          <h1>Todo App</h1>
-        </section>
-        <section className="row">
-          <input
-            value={inputValue}
-            className="col form-control me-2"
-            type="text"
-            placeholder="Add a new todo"
-            onChange={handleInputChange}
-          />
-          <div className="col-1 btn btn-primary " onClick={addToList}>
-            <img src={plusLogo} alt="plus" style={{ height: "30px" }} />
-          </div>
-        </section>
-        <section>
-          <ul style={{ "list-style-type": "none", padding: "0" }}>
-            {todolist.map((item, index) => {
-              return (
-                <li key={index}>
-                  <div className="row">
-                    <input
-                      type="text"
-                      className="col form-control me-2 mt-2"
-                      value={item} readOnly={true}
-                      style={{ backgroundColor: '#F3F1F4' }}/>
-                    <div className="col-1 btn btn-danger mt-2" onClick={() => deleteItem(index)}>
-                      <img
-                        src={deleteLogo}
-                        alt="plus"
-                        style={{ height: "25px" }}
+        <div class="m-3">
+          <section className="mt-3 mb-3">
+            <h1>Todo App</h1>
+          </section>
+          <section className="d-flex flex-row">
+            <input
+              value={inputValue}
+              className="col form-control me-2"
+              type="text"
+              placeholder="Add a new todo"
+              onChange={handleInputChange}
+            />
+            <div
+              className="col-1 btn btn-primary "
+              onClick={addToList}
+              style={{ width: "70px" }}
+            >
+              <img src={plusLogo} alt="plus" style={{ margin:"-5px",height: "40px" }} />
+            </div>
+          </section>
+          <section>
+            <ul style={{ "list-style-type": "none", padding: "0" }}>
+              {todolist.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <div className="d-flex flex-row list-item">
+                      <input
+                        type="text"
+                        className="col form-control me-2 mt-2 list-item"
+                        value={item}
+                        readOnly={true}
+                        style={{ backgroundColor: "#F3F1F4" }}
                       />
+                      <div className="col-1 btn btn-danger mt-2 delete-item" onClick={() => deleteItem(index)} style={{width: "70px"}}>
+                        <img
+                          src={deleteLogo}
+                          alt="plus"
+                          style={{ height: "25px" }}
+                        />
+                      </div>
                     </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </section>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
       </div>
     </div>
   );
